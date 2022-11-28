32ad4df01749ce2c0d9173c879de12415597a45a
Diego <diewentdiaz@gmail.com>
2022-11-29 00:10:26 +0100

    #0 Recreación de los modelos

--- Code/ecommerce/apps/account/models.py
+++ Code/ecommerce/apps/account/models.py
@@ -13,11 +13,11 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomAccountManager(BaseUserManager):
-    def validateEmail(self, email):
-        try:
-            validate_email(email)
-        except ValidationError:
-            raise ValueError(_("You must provide a valid email address"))
+    # def validateEmail(self, email):
+    #     try:
+    #         validate_email(email)
+    #     except ValidationError:
+    #         raise ValueError(_("You must provide a valid email address"))
 
     def create_superuser(self, email, name, password, **other_fields):
 
@@ -54,11 +54,24 @@ class CustomAccountManager(BaseUserManager):
 
 
 class Customer(AbstractBaseUser, PermissionsMixin):
+    class Gender(models.TextChoices):
+        MALE = "M", "Male"
+        FEMALE = "F", "Female"
+
+    id = models.autoField(primary_key=True)
     email = models.EmailField(_("email address"), unique=True)
-    name = models.CharField(max_length=150)
-    mobile = models.CharField(max_length=20, blank=True)
+    first_name = models.CharField(_("first name"), max_length=30, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    age = models.PositiveIntegerField(_("Age"), null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=Gender.choices)
+    region = models.CharField(max_length=150)
+    alergies = models.CharField(max_length=150)
+    pathologies = models.CharField(max_length=150)
+
+
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -71,40 +84,31 @@ class Customer(AbstractBaseUser, PermissionsMixin):
         verbose_name = "Accounts"
         verbose_name_plural = "Accounts"
 
-    def email_user(self, subject, message):
-        send_mail(
-            subject,
-            message,
-            "l@1.com",
-            [self.email],
-            fail_silently=False,
-        )
-
     def __str__(self):
         return self.name
 
 
-class Address(models.Model):
-    """
-    Address
-    """
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    customer = models.ForeignKey(Customer, verbose_name=_("Customer"), on_delete=models.CASCADE)
-    full_name = models.CharField(_("Full Name"), max_length=150)
-    phone = models.CharField(_("Phone Number"), max_length=50)
-    postcode = models.CharField(_("Postcode"), max_length=50)
-    address_line = models.CharField(_("Address Line 1"), max_length=255)
-    address_line2 = models.CharField(_("Address Line 2"), max_length=255)
-    town_city = models.CharField(_("Town/City/State"), max_length=150)
-    delivery_instructions = models.CharField(_("Delivery Instructions"), max_length=255)
-    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
-    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
-    default = models.BooleanField(_("Default"), default=False)
-
-    class Meta:
-        verbose_name = "Address"
-        verbose_name_plural = "Addresses"
-
-    def __str__(self):
-        return "{} Address".format(self.full_name)
+# class Address(models.Model):
+#     """
+#     Address
+#     """
+#
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     customer = models.ForeignKey(Customer, verbose_name=_("Customer"), on_delete=models.CASCADE)
+#     full_name = models.CharField(_("Full Name"), max_length=150)
+#     phone = models.CharField(_("Phone Number"), max_length=50)
+#     postcode = models.CharField(_("Postcode"), max_length=50)
+#     address_line = models.CharField(_("Address Line 1"), max_length=255)
+#     address_line2 = models.CharField(_("Address Line 2"), max_length=255)
+#     town_city = models.CharField(_("Town/City/State"), max_length=150)
+#     delivery_instructions = models.CharField(_("Delivery Instructions"), max_length=255)
+#     created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
+#     updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+#     default = models.BooleanField(_("Default"), default=False)
+#
+#     class Meta:
+#         verbose_name = "Address"
+#         verbose_name_plural = "Addresses"
+#
+#     def __str__(self):
+#         return "{} Address".format(self.full_name)
--- Code/ecommerce/apps/catalogue/models.py
+++ Code/ecommerce/apps/catalogue/models.py
@@ -5,6 +5,44 @@ from django.utils.translation import gettext_lazy as _
 from mptt.models import MPTTModel, TreeForeignKey
 
 
+
+class Department(MPTTModel):
+    name = models.CharField(max_length=255, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Medic(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="images/", blank=True, null=True, default="images/default.jpg")
+
+    department = TreeForeignKey(
+        Department, on_delete=models.CASCADE, related_name="medics"
+    )
+
+    def __str__(self):
+        return self.name
+
+class Appointment(models.Model):
+    id = models.AutoField(primary_key=True)
+    date_time = models.DateTimeField(_("Date and time of appointment"))
+    medic = models.ForeignKey(Medic, on_delete=models.CASCADE, related_name="appointments")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="appointments")
+
+    def __str__(self):
+        return f"{self.medic} - {self.date} - {self.time}"
+
+    def get_absolute_url(self):
+        return reverse("appointment-detail", kwargs={"pk": self.pk})
+
+    class Meta:
+        ordering = ["date", "time"]
+
+
+# OLD CODE #####################################################################
 class Category(MPTTModel):
     """
     Category Table implimented with MPTT.
