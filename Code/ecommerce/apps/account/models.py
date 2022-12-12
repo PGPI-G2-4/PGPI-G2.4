542c36bb9ece7ef4525c5fd9c00d1bb917e566a9
JuanAntonio4 <juanantoniomenavargas@gmail.com>
2022-12-12 18:34:45 +0100

    Resolve Code Smells

--- Code/ecommerce/apps/account/models.py
+++ Code/ecommerce/apps/account/models.py
@@ -20,48 +20,6 @@ tipo_incidencia=[
     (3, 'Otros')
 ]
 
-
-# class CustomAccountManager(BaseUserManager):
-#     # def validateEmail(self, email):
-#     #     try:
-#     #         validate_email(email)
-#     #     except ValidationError:
-#     #         raise ValueError(_("You must provide a valid email address"))
-#
-#     def create_superuser(self, email, name, password, **other_fields):
-#
-#         other_fields.setdefault("is_staff", True)
-#         other_fields.setdefault("is_superuser", True)
-#         other_fields.setdefault("is_active", True)
-#
-#         if other_fields.get("is_staff") is not True:
-#             raise ValueError("Superuser must be assigned to is_staff=True")
-#         if other_fields.get("is_superuser") is not True:
-#             raise ValueError("Superuser must be assigned to is_superuser=True")
-#
-#         if email:
-#             email = self.normalize_email(email)
-#             self.validateEmail(email)
-#         else:
-#             raise ValueError(_("Superuser Account: You must provide an email address"))
-#
-#         return self.create_user(email, name, password, **other_fields)
-#
-#     def create_user(self, email, name, password, **other_fields):
-#
-#         if email:
-#             email = self.normalize_email(email)
-#             self.validateEmail(email)
-#         else:
-#             raise ValueError(_("Customer Account: You must provide an email address"))
-#
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, name=name, **other_fields)
-#         user.set_password(password)
-#         user.save()
-#         return user
-
-
 class Customer(AbstractUser):
     class Gender(models.TextChoices):
         MALE = "M", "Male"
@@ -105,30 +63,3 @@ class Incidencia(models.Model):
 
 
 
-
-
-
-# class Address(models.Model):
-#     """
-#     Address
-#     """
-#
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     customer = models.ForeignKey(Customer, verbose_name=_("Customer"), on_delete=models.CASCADE)
-#     full_name = models.CharField(_("Full Name"), max_length=150)
-#     phone = models.CharField(_("Phone Number"), max_length=50)
-#     postcode = models.CharField(_("Postcode"), max_length=50)
-#     address_line = models.CharField(_("Address Line 1"), max_length=255)
-#     address_line2 = models.CharField(_("Address Line 2"), max_length=255)
-#     town_city = models.CharField(_("Town/City/State"), max_length=150)
-#     delivery_instructions = models.CharField(_("Delivery Instructions"), max_length=255)
-#     created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
-#     updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
-#     default = models.BooleanField(_("Default"), default=False)
-#
-#     class Meta:
-#         verbose_name = "Address"
-#         verbose_name_plural = "Addresses"
-#
-#     def __str__(self):
-#         return "{} Address".format(self.full_name)
